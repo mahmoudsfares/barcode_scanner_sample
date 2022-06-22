c93d33df66a6c6741309651fd7fd1342a6fda1d0
mahmoudsfares <msaeed@minapharm.com>
2022-06-22 11:48:27 +0200

    * Exclude get usages * Simplify code

--- lib/main.dart
+++ lib/main.dart
@@ -2,112 +2,78 @@ import 'package:barcode_scanner_sample/barcode_scanner_view.dart';
 import 'package:barcode_scanner_sample/camera_manager.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isBarcodeScannerOn = false;
+
+  final CameraManager _cameraManager = CameraManager();
+
+  Future<void> _initCamera() async {
+    _cameraManager.cameras = await availableCameras();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Barcode Scanner',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-      getPages: <GetPage> [
-        GetPage(
-          name: '/home',
-          page: () => HomeScreen(),
-          binding: HomeBinding(),
-        ),
-      ],
-    );
-  }
-}
-
-class HomeBinding extends Bindings {
-  @override
-  Future<void> dependencies() async {
-    CameraManager cameraManager = Get.put(CameraManager());
-    cameraManager.cameras = await availableCameras();
-  }
-}
-
-class HomeScreen extends GetView {
-
-  final RxBool isBarcodeScannerOn = false.obs;
-  final RxInt state = 1.obs;
-  final CameraManager _cameraManager = Get.find();
-
-  @override
-  Widget build(BuildContext context) {
+    _initCamera();
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Obx(
-                  () {
-                if(state.value == 1){
-                  return Column(
-                    children: <Widget>[
-                      const Text(
-                        'Scan QR Code',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      const Text(
-                        'Align QR code within the frame to scan',
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: <Widget>[
-                            Expanded(
-                              child: Obx(() {
-                                if (isBarcodeScannerOn.value) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(16),
-                                    child: BarcodeScannerView(
-                                      key: UniqueKey(),
-                                      cameraManager: _cameraManager,
-                                      onBarcodeDetected: (String qrCode) {
-                                        if (isBarcodeScannerOn.value) {
-                                          print(qrCode);
-                                          isBarcodeScannerOn.value = false;
-                                        }
-                                      },
-                                    ),
-                                  );
-                                } else {
-                                  return const Center(
-                                    child: Text('press scan'),
-                                  );
-                                }
-                              }),
-                            ),
-                            ElevatedButton(
-                              child: const Text('scan'),
-                              onPressed: () => isBarcodeScannerOn.value = true,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              }
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Scan QR Code',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const Text(
+                'Align QR code within the frame to scan',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: isBarcodeScannerOn
+                          ? Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: BarcodeScannerView(
+                                key: UniqueKey(),
+                                cameraManager: _cameraManager,
+                                onBarcodeDetected: (String qrCode) {
+                                  if (isBarcodeScannerOn) {
+                                    print(qrCode);
+                                    setState((){isBarcodeScannerOn = false;});
+                                  }
+                                },
+                              ),
+                            )
+                          : const Center(child: Text('press scan')),
+                    ),
+                    ElevatedButton(
+                      child: const Text('scan'),
+                      onPressed: () => setState((){isBarcodeScannerOn = true;}),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
